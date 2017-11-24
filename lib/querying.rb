374596d1512141e2a8a1fb627a5f35543e83ba37
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-24 00:59:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,15 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(*) FROM characters GROUP BY characters.species ORDER BY COUNT(*) DESC LIMIT 1;"
+  #INNER JOIN character_books ON characters.id = character_books.character_id JOIN books ON books.id = character_books.book_id WHERE
 end
 
 def select_name_and_series_subgenres_of_authors
