1aa2c1c2d99a77d037eb124e7be269c31d9ec0d5
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-24 01:58:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,5 +24,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id) FROM characters INNER JOIN character_books ON character_books.character_id = characters.id
-  GROUP BY character_books.character_id"
+  GROUP BY character_books.character_id ORDER BY DESC;"
 end
