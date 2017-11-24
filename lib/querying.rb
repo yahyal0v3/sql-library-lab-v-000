8b6e8de4d3f561ed0860b51da85d4f61e467b465
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-24 01:37:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,9 +18,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species = 'human') FROM series INNER JOIN characters ON characters.series_id = series.id
-  GROUP BY series.title ORDER BY COUNT(characters.species = 'human')
-  WHERE characters.species = 'human'  COUNT(characters.species) DESC LIMIT 1;"
+  "SELECT series.title FROM series INNER JOIN characters ON characters.series_id = series.id
+  GROUP BY series.title ORDER BY COUNT(characters.species = 'human') DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
