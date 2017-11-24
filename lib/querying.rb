90e3e08c46028935d2017d3262d246a861ff642f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-24 01:56:39 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,5 +23,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.book_id) FROM characters INNER JOIN character_books ON character_books.character_id = characters.id
+  GROUP BY character_books.character_id"
 end
