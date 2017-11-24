cf1aeec079cf5ebc55d867cddaa2eea6762d84c2
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-24 01:13:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title, characters.species FROM series INNER JOIN characters ON characters.series_id = series.id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
